4490842b69d080bbe00c0d99ce4e256cb09122e1
JenT83 <43851846+JenT83@users.noreply.github.com>
2019-04-02 20:28:44 -0400

    word cloud

---
+++ js/app.js
@@ -0,0 +1,36 @@
+function init() {
+    
+    d3.json(`/`).then(() => {
+        // Use d3 to select the panel 
+        d3.select("#word_cloud");
+            // const babynames = top50.name;
+            // const babycount = top50.count;
+    
+            // build word cloud 
+            var Canvas = require("canvas");
+        
+            var cloud = require("../");
+        
+            var words = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
+            .map(function(d) {
+            return {text: d, size: 10 + Math.random() * 90};
+            });
+        
+            cloud().size([960, 500])
+                .canvas(function() { return new Canvas(1, 1); })
+                .words(words)
+                .padding(5)
+                .rotate(function() { return ~~(Math.random() * 2) * 90; })
+                .font("Impact")
+                .fontSize(function(d) { return d.size; })
+                .on("end", end)
+                .start();
+        
+            function end(words) { console.log(JSON.stringify(words)); } 
+        
+    
+    });
+}
+
+// Initialize 
+init();
