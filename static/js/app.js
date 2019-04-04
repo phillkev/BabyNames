ba9f203adee4cfd2e08866904b7baadde1ab0409
JenT83 <43851846+JenT83@users.noreply.github.com>
2019-04-03 20:25:33 -0400

    update ref

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,34 @@
 function init() {
-    
+
     d3.json(`/`).then((wordcloud) => {
         // Use d3 to select the panel 
         d3.select("#word_cloud");
-            const babynames = wordcloud.Name;
-            const babycount = wordcloud.total_count;
-    
-            // build word cloud 
-            var Canvas = require("canvas");
-        
-            var cloud = require("../");
-        
-            var words = [babynames]
-            .map(function(d) {
-            return {text: d, size: babycount};
+        const babynames = wordcloud.Name;
+        const babycount = wordcloud.total_count;
+
+        // build word cloud 
+        var Canvas = require("canvas");
+
+        var cloud = require("../");
+
+        var words = [babynames]
+            .map(function (d) {
+                return { text: d, size: babycount };
             });
-        
-            cloud().size([960, 500])
-                .canvas(function() { return new Canvas(1, 1); })
-                .words(words)
-                .padding(5)
-                .rotate(function() { return ~~(Math.random() * 2) * 90; })
-                .font("Impact")
-                .fontSize(function(d) { return d.size; })
-                .on("end", end)
-                .start();
-        
-            // function end(words) { console.log(JSON.stringify(words)); } 
-        
-    
+
+        cloud().size([960, 500])
+            .canvas(function () { return new Canvas(1, 1); })
+            .words(words)
+            .padding(5)
+            .rotate(function () { return ~~(Math.random() * 2) * 90; })
+            .font("Impact")
+            .fontSize(function (d) { return d.size; })
+            .on("end", end)
+            .start();
+
+        function end(words) { console.log(JSON.stringify(words)); }
+
+
     });
 }
 
