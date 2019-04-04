f5aae5bab82007eea42c52f7decafcc2245061a4
JenT83 <43851846+JenT83@users.noreply.github.com>
2019-04-03 22:40:18 -0400

    word cloud

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,25 @@
 function init() {
 
-    one = {"Name": "Eric",
-    "total_count": 865555}
-
-    d3.json(`/`).then((word) => {
+    d3.json(`/cloudchartdata`).then((word) => {
         // Use d3 to select the panel 
         d3.select("#word_cloud");
-        const babynames = one.Name;
-        const babycount = one.total_count;
+        const babynames = word.Name;
+        const babycount = word.total_count;
 
         // build word cloud 
-        var Canvas = require("canvas");
-
-        var cloud = require("../");
-
         var words = [babynames]
             .map(function (d) {
                 return { text: d, size: babycount };
             });
 
-        cloud().size([960, 500])
-            .canvas(function () { return new Canvas(1, 1); })
+        d3.wordcloud()
+            .size([800, 400])
+            .selector('#word_cloud')
             .words(words)
-            .padding(5)
-            .rotate(function () { return ~~(Math.random() * 2) * 90; })
-            .font("Impact")
-            .fontSize(function (d) { return d.size; })
-            .on("end", end)
             .start();
 
-        function end(words) { console.log(JSON.stringify(words)); }
-
-
     })
-    
+
     .catch((e) => {return console.log(e)})
 
 }
